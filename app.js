8f1f96a1014a7b4c8e0278c28fc3fe391e5525b6
antac <angello_04_09@hotmail.com>
2021-08-31 18:28:43 -0500

    Ordenando el app.js

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-const div = document.querySelectorAll('#texto')
-const inputs = document.getElementById('inputs')
-let mensaje = document.getElementById('mensaje')
-let segundos = document.getElementById('segundos')
-let color = document.getElementById('color')
-let animacion = document.querySelectorAll('.rueda')
+/* asignando las variables del DOM */
+const div = document.querySelectorAll("#texto");
+const inputs = document.getElementById("inputs");
+const mensaje = document.getElementById("mensaje");
+const segundos = document.getElementById("segundos");
+const color = document.getElementById("color");
+const animacion = document.querySelectorAll(".rueda");
 
-function inputMensaje(mensaje = 'texto de prueba'){
-    for (d of div){
-        d.innerText = mensaje
-    }
+function inputMensaje(mensaje = "texto de prueba") {
+  for (d of div) {
+    d.innerText = mensaje;
+  }
 }
 
-function inputVelocidad(velocidad){
-    for (a of animacion){
-        a.style.animationDuration = `${velocidad}s`
-    }
-    console.log(`${velocidad}s`)
+function inputVelocidad(velocidad) {
+  for (a of animacion) {
+    a.style.animationDuration = `${velocidad}s`;
+  }
+  /* console.log(`${velocidad}s`) */
 }
-inputs.addEventListener('input',()=>{
-    /* segundos =segundos.valueAsNumber*100/500*/
-    let m = mensaje.value 
-    let s = segundos.valueAsNumber*100/500
-    let c = color.value
-    m ?
-    inputMensaje(m)
-    : 
-    inputMensaje();
-    inputVelocidad(s)
-    document.documentElement.style.setProperty('--color-texto', c)
-})
 
-window.addEventListener('DOMContentLoaded',()=>{
-    segundos.value = 25
-    inputMensaje()
+inputs.addEventListener("input", () => {
+  /* simplificando las variables */
+  let m = mensaje.value;
+  /* segundos =segundos.valueAsNumber*100/500*/
+  let s = (segundos.valueAsNumber * 100) / 500;
+  let c = color.value;
+  
+  /* comprobando si el input-mensaje esta vacio */
+  m ? inputMensaje(m) : inputMensaje();
+  
+  /* asignando la velocidad de la animacion */
+  inputVelocidad(s);
+  
+  /* cambiando el la variable de color de texto en root */
+  document.documentElement.style.setProperty("--color-texto", c);
+});
+
+/* llamada inicial de la web */
+window.addEventListener("DOMContentLoaded", () => {
+  segundos.value = 25;
+  inputMensaje();
 });
