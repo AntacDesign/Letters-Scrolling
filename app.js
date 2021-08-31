2cbfe42307fea8a1279086be870d1e97f6ef09a0
antac <angello_04_09@hotmail.com>
2021-08-30 20:19:23 -0500

    avanzando

---
+++ app.js
@@ -0,0 +1,7 @@
+let div = document.createElement('div');
+div.textContent = 'Nose que ponerrrrr';
+div.classList.add('texto');
+div.classList.add('rueda');
+window.addEventListener('DOMContentLoaded',()=>{
+    document.body.appendChild(div);
+});
