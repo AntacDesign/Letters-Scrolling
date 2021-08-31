3b70916ae10a3feac992f8e961f5e8650836b505
antac <angello_04_09@hotmail.com>
2021-08-31 18:11:34 -0500

    implementacion de imputs para cambiar dinamicamente el texto

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-let div = document.createElement('div');
-div.textContent = 'Te amuuu muchuu mi curazun hermusha          Te amuuu muchuu mi curazun hermusha';
-div.classList.add('texto');
-div.classList.add('rueda');
+const div = document.querySelectorAll('#texto')
+const inputs = document.getElementById('inputs')
+let mensaje = document.getElementById('mensaje')
+let segundos = document.getElementById('segundos')
+let color = document.getElementById('color')
+let animacion = document.querySelectorAll('.rueda')
+
+function inputMensaje(mensaje = 'texto de prueba'){
+    for (d of div){
+        d.innerText = mensaje
+    }
+}
+
+function inputVelocidad(velocidad){
+    for (a of animacion){
+        a.style.animationDuration = `${velocidad}s`
+    }
+    console.log(`${velocidad}s`)
+}
+inputs.addEventListener('input',()=>{
+    /* segundos =segundos.valueAsNumber*100/500*/
+    let m = mensaje.value 
+    let s = segundos.valueAsNumber*100/500
+    let c = color.value
+    m ?
+    inputMensaje(m)
+    : 
+    inputMensaje();
+    inputVelocidad(s)
+    document.documentElement.style.setProperty('--color-texto', c)
+})
+
 window.addEventListener('DOMContentLoaded',()=>{
-    document.body.appendChild(div);
+    segundos.value = 25
+    inputMensaje()
 });
